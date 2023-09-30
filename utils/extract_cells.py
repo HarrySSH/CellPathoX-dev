ad7a292d8b5329da0b9ea85d29b898440546166f
Harry Sun <ssun2@Harrys-MacBook-Pro.local.ucsf.edu>
2023-09-30 13:15:13 -0700

    add saving function

--- utils/extract_cells.py
+++ utils/extract_cells.py
@@ -113,8 +113,15 @@ class extract_annotation:
 
                 # catogory of the cell
                 category = np.unique(_mask_orig[segmented_cells])
+                # remove 0
+                category = category[category != 0]
                 # use the category with the most pixels
-                category = np.argmax(np.bincount(category))
+                # besides 0 the category should have only one value
+                if len(category) > 1:
+                    # choose the most frequent category
+                    category = np.argmax(np.bincount(category))
+                else:
+                    category = category[0]
                 
                 _center = np.mean(_center, axis = 1)
                 _center = np.round(_center)
